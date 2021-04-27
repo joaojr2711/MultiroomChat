9e67eaacce619137f607567e0b3f9c171b915dd8
João Batista <joaobat1999@gmail.com>
2021-04-27 16:23:42 -0300

    feat: render message and conversation of people

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const app = require("./config/server");
-const socketIO = require('socket.io');
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running... 🚀`);
 })
 
-socketIO.listen(server);
+const io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+io.on('connection', (socket) => {
+  console.log(`Socket Conected`);
+
+  socket.on('disconnect', () => {
+    console.log(`Socket Disconect`);
+  })
+
+
+  socket.on('messageInput', (data) => {
+
+    // Dialogs
+    socket.emit('msg', { apelido: data.apelido, message: data.message });
+
+    socket.broadcast.emit('msg', { apelido: data.apelido, message: data.message });
+
+    // Participants
+    if(parseInt(data.apelido_atualizado) == 0){
+      socket.emit('peoples', { apelido: data.apelido });
+
+      socket.broadcast.emit('peoples', { apelido: data.apelido });
+    }
+
+  });
+})
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,15 +1,17 @@
 module.exports.initChat = (application, req, res) => {
   const dataForm = req.body;
-  
-  req.assert('apelido','Nome ou apelido é obrigatório').notEmpty();
-  req.assert('apelido','Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+  req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+  req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
 
   const err = req.validationErrors();
-  
-  if(err){
+
+  if (err) {
     res.render('index', { validation: err });
     return;
   }
 
-  res.render("chat");
+  application.get('io').emit('msg', { apelido: dataForm.apelido, message: 'acabou de entrar no chat' })
+
+  res.render("chat", { dataForm: dataForm });
 };
